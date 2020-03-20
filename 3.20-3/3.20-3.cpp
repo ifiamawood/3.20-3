98c1f631dfd47ab47851ab0edcedc3dc26879715
ifiamawood <821371350@qq.com>
2020-03-20 14:52:06 +0800

    1

--- 3.20-3/3.20-3.cpp
+++ 3.20-3/3.20-3.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include<opencv.hpp>
+#include<iostream>
+
+using namespace cv;
 
 
 int main()
 {
+	cv::Mat a = imread("D:\\coin.png", 0);
+	cv::Mat b;
+	cv::Mat c, d, s, result;
+	int z;
+	cv::threshold(a, b, 0, 255, THRESH_OTSU);
+	for (int i = 1; i < z, i++) {
+		Rect d;
+		d.x = s.at<int>(i, 0);
+		d.y = s.at<int>(i, 1);
+		d.width = s.at<int>(i, 2);
+		d.height = s.at<int>(i, 3);
+		rectangle(result, d, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+	imshow("binary", );
+	imshow("result", result);
+	imshow("frame", a);
+	moveWindow("frame", 0, 20);
+	moveWindow("binary", a.cols, 20);
+	moveWindow("result", a.cols * 2, 20);
     return 0;
 }
 
